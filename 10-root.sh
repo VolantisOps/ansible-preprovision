9bae4f80557a837d7f05872c461aa8a19ec4eb4d
Ben McClure <ben.mcclure@gmail.com>
2017-06-17 13:25:36 -0500

    Simplify root script

--- 10-root.sh
+++ 10-root.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 
-hash=$(shasum /etc/ssl/certs/ca-certificates.crt | awk '{ print $1 }')
-
-echo "Installing parted..."
-pacman -Sy parted --noconfirm --needed
-
-rootsize=$(df -hm | sed -n 2p | awk '{print $2}')
-if [ $rootsize < 3000 ]; then
-    echo "You must resize your disk now and then re-run this script:"
-    echo "1. Run 'fdisk [device]'"
-    echo "2. Use the options: d [Enter] n [Enter] [Enter] [Enter] [Enter] w"
-    echo "3. Reboot or run partprobe"
-    echo "4. Run 'resize2fs [partition]'"
-    exit 1
-fi
-
-if [ $hash = "6aac3400f3ae5ed3bd736d71671c9e1ac1d379f1" ]; then
-    echo "Deleting old ca-certificates.crt file before system upgrade..."
-    rm -f /etc/ssl/certs/ca-certificates.crt
-fi
-
-echo "Installing system updates..."
-pacman -Syu --noconfirm
-
-echo "Installing sudo and setting up alarm user for sudo access..."
-pacman -Sy sudo --noconfirm --needed
-echo '%wheel ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/wheel
-usermod -G wheel alarm
-
 read -p "What hostname would you like the server to have? " hostname </dev/tty
 
 echo "Setting hostname..."
@@ -46,10 +18,5 @@ chmod 600 /root/.ssh/authorized_keys
 echo "Installing python2..."
 pacman -Sy python2 --noconfirm --needed
 
-echo "Installing build prerequisites..."
-pacman -Sy binutils make gcc fakeroot expac yajl git cower --noconfirm --needed
-
-echo "You should reboot, and optionally set a static IP designation in your router now."
-echo "Then, log in as the alarm user and run:"
-echo "'curl -s https://raw.githubusercontent.com/bmcclure/ansible-preprovision/master/20-alarm.sh | bash'"
+echo "You should now be ready to finish provisioning with Ansible."
 exit 0
